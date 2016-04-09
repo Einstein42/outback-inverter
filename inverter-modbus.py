b37d4bf82383d33723a20b44d35c8d19e4432e9a
Einsten42 <milne.james@gmail.com>
2016-04-08 23:28:42 -0500

    Initial commit

---
+++ inverter-modbus.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+""" Node Server for Polyglot 
+      by Einstein.42(James Milne)
+      milne.james@gmail.com"""
+
+from polyglot.nodeserver_api import SimpleNodeServer, PolyglotConnector
+from inverter-types import ModbusController
+
+VERSION = "0.1.1"
+
+
+class ModbusNodeServer(SimpleNodeServer):
+    controller = []
+    mbnodes = []
+
+    def setup(self):
+        manifest = self.config.get('manifest',{})
+        self.controller = ModbusController(self,'mbcontroller','Modbus Controller', True, manifest)
+        self.poly.LOGGER.info("FROM Poly ISYVER: " + self.poly.isyver)
+        self.controller._discover()
+        self.update_config()
+        
+    def poll(self):
+        pass
+
+    def long_poll(self):
+        pass
+        
+def main():
+    # Setup connection, node server, and nodes
+    poly = PolyglotConnector()
+    # Override shortpoll and longpoll timers to 5/30, once per second is excessive in this nodeserver 
+    nserver = NestNodeServer(poly, 5, 30)
+    poly.connect()
+    poly.wait_for_config()
+    poly.LOGGER.info("Modbus Interface version " + VERSION + " created. Initiating setup.")
+    nserver.setup()
+    poly.LOGGER.info("Setup completed. Running Server.")
+    nserver.run()
+    
+if __name__ == "__main__":
+    main()
